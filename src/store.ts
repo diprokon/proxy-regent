78e60eac8c7d32bf630b0b5dfcb49e37ec32ad0b
Dmytro Prokhorov <diprokon@gmail.com>
2020-09-14 10:41:07 +0300

    created dir if not exists

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,6 @@
 import { IncomingMessage, OutgoingHttpHeaders, ServerResponse } from 'http';
-import { readFile, writeFile } from 'fs';
-import { join } from 'path';
+import { existsSync, mkdirSync, readFile, writeFile } from 'fs';
+import { dirname, join } from 'path';
 import { args } from './args';
 
 export interface Res {
@@ -44,6 +44,9 @@ class Cache {
         this.cache.forEach((v, k) => {
             res[k] = v;
         });
+        if (!existsSync(dirname(this.filePath))) {
+            mkdirSync(dirname(this.filePath));
+        }
         writeFile(this.filePath, JSON.stringify(res), (err) => {
             if (err) {
                 console.error(err);
