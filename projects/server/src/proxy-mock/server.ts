bace8c864a5f21f9bb353926985596994c296a2f
Dmytro Prokhorov <diprokon@gmail.com>
2020-10-19 21:24:57 +0300

    added skiped requests; added skiped headers

--- projects/server/src/proxy-mock/server.ts
+++ projects/server/src/proxy-mock/server.ts
@@ -1,47 +1,71 @@
-import * as http from 'http';
+import { IncomingMessage } from 'http';
+import * as express from 'express';
 import * as HttpProxyServer from 'http-proxy';
-import { cache, get, set } from './store';
+import { cache, Res } from './store';
 import { args, error, info, log, say } from '../shared';
 
 export class ProxyMockServer {
     private port = parseInt(args.port, 10);
     private readonly target: string;
-    private proxy: HttpProxyServer;
+    private proxy = HttpProxyServer.createProxyServer();
+    private server = express();
 
     constructor() {
         this.target = args.target;
         if (!this.target) {
             error('Please provide target url: -t <target>');
         }
-
     }
 
     start() {
         say('Starting....');
-        this.proxy = HttpProxyServer.createProxyServer();
 
-        this.proxy.on('proxyRes', (proxyRes, req, res) => {
-            if (cache.isActive) {
-                set(req, proxyRes);
-            }
+        this.proxy.on('proxyRes', async (proxyRes, req, res) => {
+            const value = await this.getProxyData(req, proxyRes);
+            cache.set(req.url, value);
         });
 
-        const server = http.createServer((req, res) => {
-            if (cache.isActive && get(req, res)) {
-                log(`from cache -> ${req.url}`);
-            } else {
-                info(`-> ${req.url}`);
-                this.proxy.web(req, res, {
-                    target: this.target,
-                    secure: false,
-                    autoRewrite: true,
-                    changeOrigin: true
+        this.server.use((req, res, next) => {
+            const value = cache.get(req.url);
+            if (!value) {
+                next();
+                return;
+            }
+            log(`from cache -> ${req.url}`);
+            res
+                .set(value.headers)
+                .status(value.status)
+                .send(value.data);
+        });
+        this.server.use((req, res) => {
+            info(`-> ${req.url}`);
+            this.proxy.web(req, res, {
+                target: this.target,
+                secure: false,
+                autoRewrite: true,
+                changeOrigin: true
+            });
+        });
+
+        this.server
+            .listen(this.port, () => {
+                say(`Proxy listening: localhost:${this.port} -> ${this.target}`)
+            });
+    }
+
+    private async getProxyData(req: IncomingMessage, proxyRes: IncomingMessage): Promise<Res> {
+        return new Promise((resolve) => {
+            const data = [];
+            proxyRes.on('data', (d) => {
+                data.push(d);
+            });
+            proxyRes.on('close', () => {
+                resolve({
+                    data: Buffer.concat(data).toString(),
+                    headers: proxyRes.headers,
+                    status: proxyRes.statusCode
                 });
-            }
+            });
         });
-        server.listen(this.port);
-        server.on('listening', () => say(`Proxy listening: localhost:${this.port} -> ${this.target}`));
-        server.on('error', console.error);
-
     }
 }
